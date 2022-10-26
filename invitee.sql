fb1bcb3eb417a4c9792493d4b9d9326ebdf1dec0
Ramees <ramees.kr@outlook.com>
2022-10-26 16:13:46 +0530

    Add database Diagram and sql file
    Currently we can manage with only one table.

---
+++ invitee.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `prople` (
+  `id` int,
+  `name` varchar(255),
+  `house_name` varchar(255),
+  `phone_number` int,
+  `notes` varchar(255),
+  `mother_id` int,
+  `father_id` int,
+  `short_place` varchar(255),
+  `place` varchar(255),
+  `invited_ind` int
+);
