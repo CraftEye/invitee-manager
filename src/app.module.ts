e577bea2b2720dcf6d97c8f77752dd864f3d3c42
Chief-Aathil <aathilr7@gmail.com>
2022-10-31 18:34:21 +0530

    Import other modules in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,18 @@ import { DatabaseModule } from './common/database/databse.module';
 import { PersonModule } from './person/person.module';
 import { HouseModule } from './house/house.module';
 import { EventModule } from './event/event.module';
+import { FamilyModule } from './family/family.module';
+import { SampleModule } from './sample/sample.module';
 
 @Module({
-  imports: [DatabaseModule, PersonModule, HouseModule, EventModule],
+  imports: [
+    DatabaseModule,
+    PersonModule,
+    HouseModule,
+    EventModule,
+    FamilyModule,
+    SampleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
