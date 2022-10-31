8be1bcde2666496884c8bf555160653dfdb6d8e5
Chief-Aathil <aathilr7@gmail.com>
2022-10-31 18:34:00 +0530

    Use BaseService in PersonService

--- src/person/service/person.service.ts
+++ src/person/service/person.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { BaseService } from '../../common/service/base.service';
 import { Person } from '../entity/person.entity';
 
 @Injectable()
-export class PersonService {
+export class PersonService extends BaseService<Person> {
   constructor(
     @InjectRepository(Person)
     personRepo: Repository<Person>
   ) {
-    
+    super(personRepo);
   }
 }
